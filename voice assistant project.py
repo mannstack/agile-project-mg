36f84ef21ce408e5508bf20cc7d78457b84e4161
mannstack <manngarg2005@gmail.com>
2026-04-20 23:04:56 +0530

    Add files via upload

---
+++ voice assistant project.py
@@ -0,0 +1,55 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import webbrowser
+import wikipedia
+
+# Initialize engine
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        command = r.recognize_google(audio)
+        print("You said:", command)
+    except:
+        print("Sorry, could not understand.")
+        return ""
+    return command.lower()
+
+def run_assistant():
+    speak("Hello, I am your assistant")
+
+    while True:
+        command = take_command()
+
+        if "time" in command:
+            time = datetime.datetime.now().strftime("%H:%M")
+            speak("The time is " + time)
+
+        elif "open youtube" in command:
+            webbrowser.open("https://youtube.com")
+
+        elif "open google" in command:
+            webbrowser.open("https://google.com")
+
+        elif "who is" in command:
+            person = command.replace("who is", "")
+            info = wikipedia.summary(person, sentences=2)
+            speak(info)
+
+        elif "exit" in command or "stop" in command:
+            speak("Goodbye!")
+            break
+
+run_assistant()
